74f559c4db1b7f5d139691b98075a66d7d268dcf
ruivonu <ruivonu>
2018-07-21 15:07:37 -0300

    acrescentando mais exercicios aula 3

--- src/aula3/Exercicio1.java
+++ src/aula3/Exercicio1.java
@@ -1,5 +1,8 @@
 package aula3;
-
+//Crie um programa para simular uma urna de votação para exatamente 3 candidatos.  
+//Logo no início deve-se perguntar ao usuário os nomes dos candidatos. Permita votos em branco. 
+//Ao término de toda a entrada, apresente o nome, a quantidade de votos e o percentual de cada candidato. 
+//Apresente também a quantidade e o percentual dos votos em branco e quem foi o ganhador da eleição.
 public class Exercicio1 {
 	public static void main(String[] args) {
 		
---
+++ src/aula3/Exercicio2.java
@@ -0,0 +1,12 @@
+package aula3;
+//1. Apresente uma tabela contendo a evolução do valor de uma dívida ao longo dos meses e anos a partir de janeiro de 2007.
+//   Solicite o valor da dívida, a taxa de correção  e a quantidade de anos.               
+//   Por exemplo, para uma dívida de R$ 100,00 ao longo de 2 anos e com uma taxa de correção de 1.8 % ao mês:
+//2. Anos    Jan    Fev    Mar     Abr     Mai     Jun     Jul     Ago     Set     Out     Nov     Dez
+//   2007    100    101,8  103,63  105,5   107,4   109,33  111,3   113,3   115,34  117,42  119,53  121,68
+//   2008    123,87 126,1  128,37  130,68  133,03  135,43  137,87  140,35  142,87  145,45  148,06  150,73
+public class Exercicio2 {
+	public static void main(String[] args) {
+		
+	}
+}
