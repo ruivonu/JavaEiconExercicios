7aa03b47f80c979a879a289ea694676107188266
ruivonu <ruivonu>
2018-08-07 00:13:33 -0300

    alteração nos exercicio e inicio do exercicio aula7

--- src/br/com/eicon/aula7/hierarquia.java
+++ src/br/com/eicon/aula7/hierarquia.java
@@ -6,6 +6,7 @@ public class hierarquia {
 	public static void main(String[] args) {
 		Scanner inputDado = new Scanner(System.in);
 		String tipoHierarquia;
-		tipoHierarquia = inputDado.nextLine();		
+		tipoHierarquia = inputDado.nextLine();
+		
 	}
 }
